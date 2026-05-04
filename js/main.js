5d903ae0a498907606b830a998a798d6f24224f7
thaismota-tech <thaissantosdamota@gmail.com>
2026-05-04 19:30:40 +0200

    He finalizado el ejercicio 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 // EJERCICIO 1: Escribe una función llamada sumEveryOther que pueda recibir cualquier cantidad de números y devuelva la suma de todos los argumentos.
 function sumEveryOther(...numeros) {
-  return numeros.reduce((acum, num) => acum + num, 0);
+    return numeros.reduce((acum, num) => acum + num, 0);
 }
 console.log(sumEveryOther(6, 8, 2, 3, 1));
 console.log(sumEveryOther(11, 3, 12)); 
+
+// EJERCICIO 2:Escribe una función llamada addOnlyNums que pueda recibir cualquier número de argumentos (incluyendo números y strings) y retorne la suma solo de los números.
+function addOnlyNums(...numeros2) {
+    return numeros2
+    .filter(item => typeof item === 'number') //primero filtre para que me quede solamente con números
+    .reduce((acum, num) => acum + num, 0);
+}
+console.log(addOnlyNums(1, "perro", 2, 4)); 
